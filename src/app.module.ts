38b74aa0e8ff7e59ed133c5d20517964f1200a1b
raquel-garcia-17 <raqgarcia50@gmail.com>
2021-07-12 19:51:48 -0500

    label

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath:join(__dirname,'..','avatars')
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
